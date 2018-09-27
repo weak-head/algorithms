074292973b8f5c016c3649e49ae35f2dac6a965f
Oleksandr Zinchenko <zinchenko@live.com>
2018-09-27 11:28:53 -0700

    extending hash table example

--- data-structures/hash-table/separate-chaining-hash-table.py
+++ data-structures/hash-table/separate-chaining-hash-table.py
@@ -13,7 +13,7 @@ class Hashtable:
         index = self.__get_index(key)
         self._map[index].append((key, value))
 
-    # worst case O(n)
+    # worst case O(n), expected O(1)
     def remove(self, key):
         index = self.__get_index(key)
         for ix, (k, _) in enumerate(self._map[index]):
@@ -21,16 +21,25 @@ class Hashtable:
                del self._map[index][ix]
                break
 
+    # worst case O(n), expected O(1)
+    def contains(self, key):
+        index = self.__get_index(key)
+        for (k, _) in self._map[index]:
+            if key == k:
+                return True
+        return False
+
     def __get_index(self, str_object):
         return Hashtable.get_hash(str_object) % self._capacity
 
     def __str__(self):
-        srep = ''
+        srep = '<---->\n'
         for ix, bucket in enumerate(self._map):
             srep += str(ix) + ' -> ' + str(bucket) + '\n'
+        srep += '<---->\n'
         return srep
 
-    # O(k), where k is string length
+    # O(k), where k is the length of the string
     @staticmethod
     def get_hash(str_object):
         hash = 0
@@ -41,6 +50,7 @@ class Hashtable:
 if __name__ == '__main__':
     h = Hashtable(7)
     print(h)
+    print('contains "abc":', h.contains('abc'), '\n')
 
     h.insert('abc', 1)
     h.insert('abb', 2)
@@ -49,9 +59,11 @@ if __name__ == '__main__':
     h.insert('zz', 5)
     h.insert('zfa', 6)
     print(h)
+    print('contains "abc":', h.contains('abc'), '\n')
 
     h.remove('zfa')
     print(h)
 
     h.remove('abc')
     print(h)
+    print('contains "abc":', h.contains('abc'), '\n')
