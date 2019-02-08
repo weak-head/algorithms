5c81573d2355b330a8e2df39aa97f3e010ea1184
Oleksandr Zinchenko <zinchenko@live.com>
2019-02-07 20:35:14 -0800

    Detect loop in linked list

---
+++ data-structures/list/alg/loop-in-linked-list.py
@@ -0,0 +1,51 @@
+
+class Node:
+    def __init__(self, data, next = None):
+        self.data = data
+        self.next = next
+
+def mk_linked_loop():
+    """
+    Creates the linked list with a loop:
+        1 -> 2 -> 3 -> 4 -> 5
+                  ^         |
+                   ---------
+    """
+    e5 = Node(5)
+    e4 = Node(4, e5)
+    e3 = Node(3, e4)
+    e2 = Node(2, e3)
+    e1 = Node(1, e2)
+    e5.next = e3
+    return e1
+
+# O(n^2)
+def has_loop(head: Node):
+    if head is None:
+        return False
+
+    el, step = head.next, 1
+    while el is not None:
+        p, c = head, 0
+
+        # searching for the current node,
+        # (starting from the head) counting steps
+        while p != el:
+            p = p.next
+            c += 1
+
+        # the walked distance is not the same -> loop
+        if c != step:
+            return True
+        else:
+            el = el.next
+            step += 1
+
+    return False
+
+if __name__ == '__main__':
+    ll = mk_linked_loop()
+    nl = Node(1, Node(2, Node(3, Node(4, Node(5)))))
+
+    assert(has_loop(ll))
+    assert(not has_loop(nl))
