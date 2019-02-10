5a68f294bfc3ec29fafbf3b5ce415e547d03c556
Oleksandr Zinchenko <zinchenko@live.com>
2019-02-09 23:25:52 -0800

    Insert item into heap

--- data-structures/heap/heap.cc
+++ data-structures/heap/heap.cc
@@ -1,4 +1,5 @@
 #include "heap.h"
+#include <iostream>
 
 template<typename T>
 Heap<T>::Heap() : vec_(), size_(0) {
@@ -12,12 +13,18 @@ Heap<T>::~Heap() {
 
 template<typename T>
 void Heap<T>::Insert(T item) {
-
+  if (size_ == vec_.size())
+    vec_.push_back(item);
+  else
+    vec_[size_] = item;
+  bubble_up(size_++);
 }
 
 template<typename T>
 T Heap<T>::Min() {
-  return NULL;
+  if (size_ == 0)
+    return NULL;
+  return vec_[0];
 }
 
 template<typename T>
@@ -32,19 +39,35 @@ void Heap<T>::heapify() {
 
 template<typename T>
 void Heap<T>::bubble_up(int index) {
-
+  while (index > 0 && vec_[index] < vec_[parent(index)]) {
+    std::swap(vec_[index], vec_[parent(index)]);
+    index = parent(index);
+  }
 }
 
 template<typename T>
 int Heap<T>::parent(int index) {
-  return 0;
+  return (index >> 1);
 }
 
 template<typename T>
 int Heap<T>::young_child(int index) {
-  return 0;
+  return (index << 1) + 1;
 }
 
 int main() {
+  Heap<int> heap;
+  heap.Insert(10);
+  heap.Insert(17);
+  heap.Insert(13);
+  heap.Insert(8);
+  heap.Insert(19);
+  heap.Insert(7);
+  std::cout << "Min item: " << heap.Min() << std::endl;
 
+  heap.Insert(4);
+  std::cout << "Min item: " << heap.Min() << std::endl;
+
+  heap.Insert(1);
+  std::cout << "Min item: " << heap.Min() << std::endl;
 }
