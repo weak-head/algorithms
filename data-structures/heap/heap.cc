ce9d8e5d488f4bae6b4d696189e62529550a5184
Oleksandr Zinchenko <zinchenko@live.com>
2019-02-09 21:05:30 -0800

    Heap skeleton

---
+++ data-structures/heap/heap.cc
@@ -0,0 +1,50 @@
+#include "heap.h"
+
+template<typename T>
+Heap<T>::Heap() : vec_(), size_(0) {
+
+}
+
+template<typename T>
+Heap<T>::~Heap() {
+
+}
+
+template<typename T>
+void Heap<T>::Insert(T item) {
+
+}
+
+template<typename T>
+T Heap<T>::Min() {
+  return NULL;
+}
+
+template<typename T>
+T Heap<T>::ExtractMin() {
+  return NULL;
+}
+
+template<typename T>
+void Heap<T>::heapify() {
+
+}
+
+template<typename T>
+void Heap<T>::bubble_up(int index) {
+
+}
+
+template<typename T>
+int Heap<T>::parent(int index) {
+  return 0;
+}
+
+template<typename T>
+int Heap<T>::young_child(int index) {
+  return 0;
+}
+
+int main() {
+
+}
---
+++ data-structures/heap/heap.h
@@ -0,0 +1,29 @@
+#ifndef HEAP_H_
+#define HEAP_H_
+
+#include <vector>
+
+template<typename T>
+class Heap {
+ public:
+  Heap();
+  ~Heap();
+
+  void Insert(T item);
+
+  T Min();
+  T ExtractMin();
+
+  int Size() const { return size_ };
+
+ private:
+  void heapify();
+  void bubble_up(int index);
+  int parent(int index);
+  int young_child(int index);
+
+  std:vector<T> vec_;
+  int size_;
+};
+
+#endif
