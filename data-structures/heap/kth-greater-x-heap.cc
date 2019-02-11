a112373cbaa7897f2c54cc8cfab0b7f305da436c
Oleksandr Zinchenko <zinchenko@live.com>
2019-02-10 22:37:23 -0800

    Heap compare, k-th with x

--- data-structures/heap/kth-greater-x-heap.cc
+++ data-structures/heap/kth-greater-x-heap.cc
@@ -1,2 +1,15 @@
-#include "heap.h"
+#include "kth-greater-x-heap.h"
 
+#include <vector>
+
+int heap_compare(std::vector<int> heap, int index, int kth, int x) {
+  if (index > heap.size() || kth <= 0)
+    return kth;
+
+  if (heap[index] < x) {
+    kth = heap_compare(heap, (index << 1),     kth - 1, x);
+    kth = heap_compare(heap, (index << 1) + 1, kth,     x);
+  }
+
+  return kth;
+};
---
+++ data-structures/heap/kth-greater-x-heap.h
@@ -0,0 +1,8 @@
+#ifndef KTH_GREATER_X_HEAP_H_
+#define KTH_GREATER_X_HEAP_H_
+
+#include <vector>
+
+int heap_compare(std::vector<int> heap, int index, int kth, int x);
+
+#endif
