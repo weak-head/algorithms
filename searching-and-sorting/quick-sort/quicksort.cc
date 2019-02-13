de019ebaf796400df68a263083ad2d3ba72b11fb
Oleksandr Zinchenko <zinchenko@live.com>
2019-02-12 23:58:19 -0800

    Quicksort

--- searching-and-sorting/quick-sort/quicksort.cc
+++ searching-and-sorting/quick-sort/quicksort.cc
@@ -3,15 +3,20 @@
 
 typedef std::vector<int> Array;
 
-/*
+int partition(Array& a, int l, int r) {
+  int last_min = l, pivot = r;
 
-*/
-void quicksort(Array& a) {
-  quicksort(a, 0, a.size() - 1);
+  for (int i = l; i < r; i++)
+    if (a[i] < a[pivot])
+      std::swap(a[i], a[last_min++]);
+
+  std::swap(a[last_min], a[pivot]);
+
+  return last_min;
 }
 
 void quicksort(Array& a, int l, int r) {
-  if (r < l)
+  if (r <= l)
     return;
 
   int p = partition(a, l, r);
@@ -19,10 +24,20 @@ void quicksort(Array& a, int l, int r) {
   quicksort(a, p + 1, r);
 }
 
-int partition(Array& a, int l, int r) {
+/*
 
+*/
+void quicksort(Array& a) {
+  quicksort(a, 0, a.size() - 1);
 }
 
 int main() {
+  Array a { 12, 1, 2, 7, 13, 20, 9, 0, 42, 17, 5, 8, 4 };
 
+  quicksort(a);
+
+  for (auto i : a)
+    std::cout << i << " ";
+
+  std::cout << std::endl;
 }
