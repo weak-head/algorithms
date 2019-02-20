d48d6b12083a34824c4b0d4c855e789929c29cf8
Oleksandr Zinchenko <zinchenko@live.com>
2019-02-20 12:39:24 -0800

    Python implementation of the merge sort

--- searching-and-sorting/merge-sort/merge-sort.py
+++ searching-and-sorting/merge-sort/merge-sort.py
@@ -19,12 +19,23 @@ def merge(a, l, m , r):
     for i in range (r, m-1, -1):
         rq.append(a[i])
 
-    for i in range(l, r + 1):
+    for i in range(l, r+1):
         a[i] = lq.pop() if (lq[len(lq)-1] < rq[len(rq)-1]) else rq.pop()
 
+def _mergesort(a, l, r):
+    if (l >= r):
+        return
+
+    pivot = (l + r) >> 1
+    _mergesort(a, l, pivot)
+    _mergesort(a, pivot + 1, r)
+    merge(a, l, pivot + 1, r)
+
+def mergesort(a):
+    _mergesort(a, 0, len(a) - 1)
 
 if __name__ == '__main__':
     a = [0, 2, 4, 6, 8, 1, 3, 5, 7]
-    merge(a, 0, 5, 8)
+    mergesort(a)
     print(a)
 
