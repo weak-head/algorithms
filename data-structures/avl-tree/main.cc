dd25f8d8c3d8557e913e2d10f33ede6ccd40630b
Oleksandr Zinchenko <zinchenko@live.com>
2019-02-23 10:29:58 -0800

    Avl: generate random array of custom length, with explicit value boundaries

--- data-structures/avl-tree/main.cc
+++ data-structures/avl-tree/main.cc
@@ -6,23 +6,23 @@
 
 using namespace std;
 
-vector<int> generate_random_array() {
+vector<int> generate_random_array(int num = 1000, int min = 1, int max = 1000) {
   random_device rnd_device;
   mt19937 mersenne_engine { rnd_device() };
-  uniform_int_distribution<int> dist {1, 1000};
+  uniform_int_distribution<int> dist {min, max};
 
   auto gen = [&dist, &mersenne_engine]() {
     return dist(mersenne_engine);
   };
 
-  vector<int> vec(500);
+  vector<int> vec(num);
   generate(begin(vec), end(vec), gen);
 
   return vec;
 }
 
 int main(int argc, char **argv) {
-  vector<int> a = generate_random_array();
+  vector<int> a = generate_random_array(100);
   avl::Avl<int> avl;
 
   for (auto i : a)
