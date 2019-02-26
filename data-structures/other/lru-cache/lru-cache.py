93a616b4eb2efd562ecbbf578ab259bd07991398
Oleksandr Zinchenko <zinchenko@live.com>
2019-02-25 23:12:03 -0800

    LRU cache: get item

--- data-structures/other/lru-cache/lru-cache.py
+++ data-structures/other/lru-cache/lru-cache.py
@@ -1,16 +1,48 @@
 '''
     Least Recently Used Cache
+    https://en.wikipedia.org/wiki/Cache_replacement_policies#Least_recently_used_(LRU)
 '''
 
 class LRUCache:
+    class DLinkNode:
+        def __init__(self, key: int, value: int, prev = None, next = None):
+            self.prev  = prev
+            self.next  = next
+            self.key   = key
+            self.value = value
+
     def __init__(self, capacity: int):
-        pass
+        self._keymap    = {}
+        self._capacity  = capacity
+        self._count     = 0
+        self._head      = self.DLinkNode(None, None)
+        self._tail      = self.DLinkNode(None, None)
+        self._head.next = self._tail
+        self._tail.prev = self._head
 
     def get(self, key: int) -> int:
+        if key not in self._keymap:
+            return -1
+        node = self._keymap[key]
+        self._move_to_front(node)
+        return node.value
+
+    def put(self, key: int, value: int) -> None:
         pass
 
-    def set(self, key: int, value: int) -> None:
-        pass
+    def _move_to_front(self, node: LRUCache.DLinkNode):
+        '''
+            Move the node to the front of the doubly linked list.
+            O(1)
+        '''
+        # detach node
+        node.prev.next = node.next
+        node.next.prev = node.prev
+        # attach to head
+        node.next = self._head.next
+        node.prev = self._head
+        self._head.next.prev = node
+        self._head.next = node
 
 if __name__ == '__main__':
-    pass
+    cache = LRUCache(5)
