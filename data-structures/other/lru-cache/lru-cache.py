3b3983fc76e9410b0c49d28445a8018e50a871e5
Oleksandr Zinchenko <zinchenko@live.com>
2019-02-25 23:39:24 -0800

    LRU cache: put value

--- data-structures/other/lru-cache/lru-cache.py
+++ data-structures/other/lru-cache/lru-cache.py
@@ -12,6 +12,8 @@ class LRUCache:
             self.value = value
 
     def __init__(self, capacity: int):
+        if capacity == 0:
+            raise Exception('Capacity cannot be zero')
         self._keymap    = {}
         self._capacity  = capacity
         self._count     = 0
@@ -21,6 +23,10 @@ class LRUCache:
         self._tail.prev = self._head
 
     def get(self, key: int) -> int:
+        '''
+            Get value from LRU Cache
+            O(1)
+        '''
         if key not in self._keymap:
             return -1
         node = self._keymap[key]
@@ -28,16 +34,41 @@ class LRUCache:
         return node.value
 
     def put(self, key: int, value: int) -> None:
-        pass
+        '''
+            Put value into LRU Cache
+            O(1)
+        '''
+        # the key/value is in the cache,
+        # we only need to move it to front of DList
+        if key in self._keymap:
+            node = self._keymap[key]
+            node.value = value
+            self._move_to_front(node)
+        # the key/value is not in the cache
+        else:
+            if self._count < self._capacity:
+                node = self.DLinkNode(key, value)
+                self._move_to_front(node, detach = False)
+                self._keymap[key] = node
+                self._count += 1
+            # cache is full
+            else:
+                lru_node = self._tail.prev
+                del self._keymap[lru_node.key]
+                lru_node.key   = key
+                lru_node.value = value
+                self._move_to_front(lru_node)
+                self._keymap[key] = lru_node
 
-    def _move_to_front(self, node: LRUCache.DLinkNode):
+    def _move_to_front(self, node: DLinkNode, detach: bool = True):
         '''
             Move the node to the front of the doubly linked list.
             O(1)
         '''
         # detach node
-        node.prev.next = node.next
-        node.next.prev = node.prev
+        if detach:
+            node.prev.next = node.next
+            node.next.prev = node.prev
         # attach to head
         node.next = self._head.next
         node.prev = self._head
